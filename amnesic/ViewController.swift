9108b8e03538ac3307c6983d5980ccc4648685b3
Nimitpamnani <C0764944@mylambton.ca>
2020-05-15 21:32:33 -0400

    Initial Commit

---
+++ amnesic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  amnesic
+//
+//  Created by Nimit on 2020-05-15.
+//  Copyright © 2020 Nimit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
